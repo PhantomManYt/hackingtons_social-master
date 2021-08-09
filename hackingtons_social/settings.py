83059f8c8fde4fa0a5cd710ada6b6ec49e6b910c
PhantomManYt <64277260+PhantomManYt@users.noreply.github.com>
2021-08-09 08:58:13 -0700

    a

--- hackingtons_social/settings.py
+++ hackingtons_social/settings.py
@@ -140,7 +140,7 @@ GS_PROJECT_ID = 'socializer-322404'
 GS_BUCKET_NAME = 'socializerbucket'
 MEDIA_ROOT = 'media/'
 UPLOAD_ROOT = 'media/uploads'
-MEDIA_URL = 'https://storage.googleapis.com/{}/'.format(GS_BUCKET_NAME)
+MEDIA_URL = 'https://storage.googleapis.com/%7B%7D/'.format(GS_BUCKET_NAME)
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
